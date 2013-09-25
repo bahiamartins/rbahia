2bcc254eb3dd975286f5ed8a51f222bd1619f27d
bahiamartins <bahiamartins@hotmail.com>
2013-09-25 18:29:03 -0300

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rbahia::Application.config.secret_key_base = 'a676d8aa11dbd780295ecff334949c3b185b21ce2b07792738f48245b2afcfccc1745cba53c18d3c89b93e7cebe2839b551980b074c1f605d797979266212625'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rbahia::Application.config.session_store :cookie_store, key: '_rbahia_session'
